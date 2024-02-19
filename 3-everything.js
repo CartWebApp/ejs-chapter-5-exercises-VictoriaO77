e6b4c3b94f9b259679aa9b908029e56e316c4b55
VictoriaO77 <143848611+VictoriaO77@users.noreply.github.com>
2024-02-19 15:39:39 -0800

    Update 3-everything.js

--- 3-everything.js
+++ 3-everything.js
@@ -1,7 +1,14 @@
-function every(array, test) {
-    // Your code here.
+function every(array, predicate) {
+    for (let element of array) {
+      if (!predicate(element)) return false;
+    }
+    return true;
   }
-  
+
+  function every2(array, predicate) {
+    return !array.some(element => !predicate(element));
+  }
+
   console.log(every([1, 3, 5], n => n < 10));
   // → true
   console.log(every([2, 4, 16], n => n < 10));
